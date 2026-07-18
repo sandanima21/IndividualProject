45680413d8dd201495001452aab127e6c96b3041
sandanima21 <sandanimahae.22@uom.lk>
2026-07-18 09:58:33 +0530

    Remove reCAPTCHA Enterprise init to fix CAPTCHA_CHECK_FAILED on Vercel

--- kukihabun/src/firebase.js
+++ kukihabun/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, initializeRecaptchaConfig } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBoSmJxvC2m09XNkuijKddA0EuawcIOVZo",
@@ -13,5 +13,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-initializeRecaptchaConfig(auth).catch(() => {});
